9be721282336ce472884137c6e119ec47eba0567
soukoli <soukoli@users.noreply.github.com>
2026-04-08 00:30:34 +0200

    chore: simplify icons to recommended PWA sizes only

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body className="antialiased">
